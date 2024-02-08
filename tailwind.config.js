22ffc0fd6df76d218a20a89aa8e8ffce3395347f
krishna sai ram <74918581+krishcodz@users.noreply.github.com>
2024-02-08 22:30:25 +0530

    intial commit

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+import daisyui from "daisyui"
+
+const darkModeConfig = {
+  darkMode: "class",
+  theme: {
+    extend: {
+      backgroundColor: {
+        dark: "#333",
+      },
+      textColor: {
+        dark: "#fff",
+      },
+    },
+  },
+}
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [daisyui],
+  daisyui: {
+    themes: ["light"],
+  },
+  ...darkModeConfig, // Merge the dark mode configuration
+}
