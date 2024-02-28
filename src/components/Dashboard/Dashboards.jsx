ed78b2e64567cd0ac60d5153c9d87866c53e44e4
krishna sai ram <74918581+krishcodz@users.noreply.github.com>
2024-02-28 22:52:29 +0530

    dashboard - added new items

--- src/components/Dashboard/Dashboards.jsx
+++ src/components/Dashboard/Dashboards.jsx
@@ -3,6 +3,10 @@ import Card from "./Card"
 import WelcomeGreeting from "../Greeting/WelcomeGreeting"
 import MyChart from "./MyChart"
 import { LineChart } from "./LineChart"
+import { Typography } from "@mui/material"
+import { FaArrowTrendUp, FaArrowTrendDown } from "react-icons/fa6"
+import { CircularProgressbar } from "react-circular-progressbar"
+import "react-circular-progressbar/dist/styles.css"
 
 const Dashboard = () => {
   const [showWelcomePopup, setShowWelcomePopup] = useState(false)
@@ -37,6 +41,20 @@ const Dashboard = () => {
       price: "224,74",
       css: "bg-rose-50 text-rose-500",
     },
+    {
+      id: 5,
+      name: "Payroll",
+      sub: "This Month",
+      price: "474,45",
+      css: "bg-green-50 text-green-500",
+    },
+    {
+      id: 6,
+      name: "Reports",
+      sub: "This Month",
+      price: "845",
+      css: "bg-blue-50 text-blue-500",
+    },
   ]
 
   // Function for welcome popup
@@ -59,24 +77,52 @@ const Dashboard = () => {
         {/* Welcome Popup */}
         <WelcomeGreeting showWelcomePopup={showWelcomePopup} closeWelcomePopup={closeWelcomePopup} />
         <div className="sm:grid sm:grid-cols-10 grid grid-flow-row gap-8">
-          <div className="col-span-8">
+          <div className="col-span-12">
             <section className="">
-              <div className="grid sm:grid-cols-4 grid-flow-row pb-6 gap-6">
+              <div className="grid sm:grid-cols-6 grid-flow-row pb-6 gap-6">
                 {Incomes.map(obj => {
                   return <Card data={obj} key={obj.id}></Card>
                 })}
               </div>
             </section>
-            <section className=" h-auto  rounded-md border border-gray-100 bg-gray-100">
-              <p className="py-7 px-2 font-bold">Weekly Income</p>
-              <div className="mt-[-3rem]"></div>
-              <MyChart />
-            </section>
+            <div className="flex justify-between gap-7">
+              <section className="h-auto w-[65%] rounded-md border border-gray-100 bg-gray-100">
+                <p className="py-7 px-2 font-bold">Weekly Income</p>
+                <div className="mt-[-3rem]"></div>
+                <MyChart />
+              </section>
+              <div className="w-[35%] flex flex-col gap-10 rounded-lg">
+                <div className="flex h-[50%] w-full justify-around items-center bg-slate-100">
+                  <div className="w-[50%]">
+                    <Typography style={{ fontSize: "20px", paddingBottom: "18px" }}>Yearly Breakup</Typography>
+                    <Typography>$ 36,358</Typography>
+                    <div className="flex gap-5 items-center py-2">
+                      <FaArrowTrendUp />
+                      <Typography>+9% last year</Typography>
+                    </div>
+                  </div>
+                  <div className="w-[20%]">
+                    <CircularProgressbar value={75} />
+                  </div>
+                </div>
+                <div className="flex h-[50%] justify-between items-center p-4 bg-slate-100">
+                  <div className="w-[50%]">
+                    <Typography style={{ fontSize: "20px", paddingBottom: "18px" }}>Monthly Earnings</Typography>
+                    <Typography>$ 6,820</Typography>
+                    <div className="flex gap-5 items-center py-2">
+                      <FaArrowTrendDown />
+                      <Typography>+9% last year</Typography>
+                    </div>
+                  </div>
+                  <div className="w-[20%]">
+                    <CircularProgressbar value={55} />
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
-          <div className="sm:col-span-2 col-span-8 bg-blue-50  overflow-hidden h-[30rem]"></div>
-
         </div>
-          <LineChart />
+        <LineChart />
       </section>
     </>
   )
