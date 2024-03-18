8a4a7b287737514a2a932c844997d3d97bfa96af
devan74 <raghavadeva1903@gmail.com>
2024-03-18 19:23:26 +0530

    Account Setting issues fix

--- src/components/Setting/RowNavigation.jsx
+++ src/components/Setting/RowNavigation.jsx
@@ -5,7 +5,7 @@ import { FaHome, FaUser } from "react-icons/fa"
 const RowNavigation = () => {
   return (
     <div className="flex justify-left gap-5 text-[14px] mb-5">
-      <Link to="/" className="flex items-center p-2 border rounded-md">
+      <Link to="/" className="flex items-center p-2 border rounded-md hover:bg-blue-200">
         <FaHome className="h-4 w-4 mr-2" />
         <span className="">Dashboard</span>
       </Link>
