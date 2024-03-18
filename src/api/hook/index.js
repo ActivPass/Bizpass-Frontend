b64542f7ff62582f4925c32096289c1e333da584
Pugazh <aakashit13@gmail.com>
2024-03-18 23:50:09 +0530

    hooks updated for change password and update patch user req

--- src/api/hook/index.js
+++ src/api/hook/index.js
@@ -1,2 +1,3 @@
 export { useLoginMutation, useLogoutMutation } from "./auth"
 export { useForgotPasswordMutation, useResetPasswordMutation, useVerifyOtpMutation } from "./auth/fogotPassword"
+export { useUpdateUserMutation, useChangePasswordMutation } from "./user"
