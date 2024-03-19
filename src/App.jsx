48c711d67092fccdf5816d98d921efc35f53b0f5
Pugazh <aakashit13@gmail.com>
2024-03-19 21:33:35 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -112,72 +112,10 @@ const route = createBrowserRouter(
 )
 
 const App = () => {
-  const [isConnected, setIsConnected] = useState(socket.connected)
-  const [fooEvents, setFooEvents] = useState([])
-
   useEffect(() => {
-    function onConnect() {
-      setIsConnected(true)
-    }
-
-    function onDisconnect() {
-      setIsConnected(false)
-    }
-
-    function onFooEvent(value) {
-      setFooEvents(previous => [...previous, value])
-    }
-
-    socket.on("connect", onConnect)
-    socket.on("disconnect", onDisconnect)
-    socket.on("foo", onFooEvent)
-
-    return () => {
-      socket.off("connect", onConnect)
-      socket.off("disconnect", onDisconnect)
-      socket.off("foo", onFooEvent)
-    }
-  }, [])
-
-  function connect() {
     socket.connect()
-  }
-
-  function disconnect() {
-    socket.disconnect()
-  }
-
-  const [value, setValue] = useState("")
-  const [isLoading, setIsLoading] = useState(false)
-
-  function onSubmit(event) {
-    event.preventDefault()
-    setIsLoading(true)
-
-    socket.timeout(5000).emit("create-something", value, () => {
-      setIsLoading(false)
-    })
-  }
-  return (
-    <main>
-      <p>State: {"" + isConnected}</p>
-      <ul>
-        {fooEvents.map((event, index) => (
-          <li key={index}>{event}</li>
-        ))}
-      </ul>
-      <button onClick={connect}>Connect</button>
-      <button onClick={disconnect}>Disconnect</button>
-      <form onSubmit={onSubmit}>
-        <input onChange={e => setValue(e.target.value)} />
-
-        <button type="submit" disabled={isLoading}>
-          Submit
-        </button>
-      </form>
-    </main>
-  )
-  // return <RouterProvider router={route} />
+  }, [])
+  return <RouterProvider router={route} />
 }
 
 export default App
