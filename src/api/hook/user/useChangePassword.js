81a47aefa3d3577702a64b39c7fd7e335604262d
Pugazh <aakashit13@gmail.com>
2024-03-20 22:51:47 +0530

    changepwd section seperated and added integration

--- src/api/hook/user/useChangePassword.js
+++ src/api/hook/user/useChangePassword.js
@@ -1,10 +1,10 @@
 import customAxios from "../../customAxios"
 import { useMutation } from "@tanstack/react-query"
-
+import { toast } from "react-toastify"
 export const useChangePasswordMutation = () => {
   const changePassword = async changePasswordRequestObj => {
     try {
-      const response = await customAxios.post(`/user/changepassword`, changePasswordRequestObj)
+      const response = await customAxios.post(`/users/changepassword`, changePasswordRequestObj)
       return response
     } catch (err) {
       return err.response
@@ -14,7 +14,16 @@ export const useChangePasswordMutation = () => {
     mutationFn: changePassword,
     mutationKey: "Change Password User",
     onSuccess: data => {
-      console.log(data)
+      if (data?.status === 200) {
+        toast.success(data.data.message)
+      } else if (data?.status === 400) {
+        toast.warn(data.data.message)
+      } else {
+        toast.error("Something unexpected happened, Please try again later")
+      }
+    },
+    onError: () => {
+      toast.error("Something unexpected happened, Please try again later")
     },
   })
 }
